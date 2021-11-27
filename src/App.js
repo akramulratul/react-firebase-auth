7d40260ffec1dc9532c1cae294b91f441a7fb708
akramulratul <akramulratul@gmail.com>
2021-11-27 15:37:22 +0600

    form making done

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import "./App.css";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import initializeAuthentication from "./Firebase/firebase.init";
+
+initializeAuthentication();
+const googleProvider = new GoogleAuthProvider();
+
+const auth = getAuth();
+
+const handleGoogleSignIn = () => {
+  signInWithPopup(auth, googleProvider).then((res) => {
+    const user = res.user;
+    console.log(user);
+  });
+};
+
+const handleRegistration = (e) => {
+  e.preventDefault();
+  console.log("registration");
+};
+function App() {
+  return (
+    <div className="App">
+      <form onSubmit={handleRegistration}>
+        <h3>Please Register</h3>
+        <label htmlFor="email">Email:</label>
+        <input type="text" name="email" />
+        <br />
+        <label htmlFor="email">Pass:</label>
+        <input type="password" name="password" />
+        <br />
+        <input type="submit" value="Register" />
+      </form>
+      <br />
+      <br />
+      <button onClick={handleGoogleSignIn}>Google sign in</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAjTViOOEXxetS7k4zHc-9F3N5B5YXSJFU",
+  authDomain: "react-firebase-authentic-d429a.firebaseapp.com",
+  projectId: "react-firebase-authentic-d429a",
+  storageBucket: "react-firebase-authentic-d429a.appspot.com",
+  messagingSenderId: "456929866120",
+  appId: "1:456929866120:web:7731c3f18d79bfa46b0a3d",
+};
+
+export default firebaseConfig;
